645ac5a0ccac32c99a17799bd0a417ad6d06b78d
DARKozDARKA <kalinkovgoha04@gmail.com>
2024-11-30 20:24:41 +0200

    UpdatedCameraFollower (Experimental)

--- Assets/CodeBase/Logic/Camera/CameraFollower.cs
+++ Assets/CodeBase/Logic/Camera/CameraFollower.cs
@@ -8,9 +8,15 @@ public class CameraFollower : MonoBehaviour
     private IPlayerProvider _playerProvider;
     private Player _player;
     private bool _isPlayerInitialized;
+
+    [SerializeField]
+    private Camera _camera;
     
     [SerializeField]
     private Vector3 _offset;
+    
+    [SerializeField]
+    private Vector2 _gridOffset;
 
     [Inject]
     public void Construct(IPlayerProvider playerProvider)
@@ -37,6 +43,23 @@ public class CameraFollower : MonoBehaviour
         if (!_isPlayerInitialized)
             return;
 
-        transform.position = _player.transform.position + _offset;
+        SnapToPlayer();
+    }
+    
+    
+    private void SnapToPlayer()
+    {
+        float cameraHeight = _camera.orthographicSize * 2f;
+        float cameraWidth = cameraHeight * _camera.aspect;
+
+        Vector3 targetPosition = _player.transform.position + _offset;
+
+        float snappedX = Mathf.Floor(targetPosition.x / cameraWidth) * cameraWidth + cameraWidth / 2f;
+        float snappedY = Mathf.Floor(targetPosition.y / cameraHeight) * cameraHeight + cameraHeight / 2f;
+        
+        snappedX += _gridOffset.x * cameraWidth;
+        snappedY += _gridOffset.y * cameraHeight;
+
+        transform.position = new Vector3(snappedX, snappedY, targetPosition.z);
     }
 }
